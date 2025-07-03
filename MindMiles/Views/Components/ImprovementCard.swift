408383e84870781a7a6856ac646de595e054dcdf
janikWa <janik.wahrheit@online.de>
2025-07-03 18:59:25 +0200

    add mood chart

---
+++ MindMiles/Views/Components/ImprovementCard.swift
@@ -0,0 +1,8 @@
+//
+//  ImprovementCard.swift
+//  MindMiles
+//
+//  Created by Janik Wahrheit on 03.07.25.
+//
+
+import Foundation
---
+++ MindMiles/Views/Components/MoodChart.swift
@@ -0,0 +1,8 @@
+//
+//  MoodChart.swift
+//  MindMiles
+//
+//  Created by Janik Wahrheit on 03.07.25.
+//
+
+import Foundation
